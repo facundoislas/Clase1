c2b4d09ac36257056b729d2ba7764f2cd1f1e518
Facundo <facu.islas99@gmail.com>
2017-03-23 00:49:55 -0300

    Se suma ejercicio 2 fechas

--- ejercicios_parte1.php
+++ ejercicios_parte1.php
@@ -1,7 +1,6 @@
-
 <?php
 //ej1
-echo "--------------Ejercicio1--------------<br><br>";
+echo "--------------Ejercicio 1--------------<br><br>";
 $suma=0;
 $cont=1;
 
@@ -17,8 +16,61 @@ while($suma<1000)
 
 echo "<br>Sume ".$cont." numeros";
 
+//ej2
+echo "<br><br>--------------Ejercicio 2--------------<br><br>";
+
+date_default_timezone_get();
+echo date("D M j Y")."<br>";
+echo date("F j, Y")."<br>";
+echo date("d-m-Y")."<br>";
+
+switch (date("m")) {
+	case '1':
+	case '2':
+		echo "es verano";
+		break;
+	case '3':
+		if(date("d")>20)
+			echo "es otoño";
+		else
+			echo "es verano";
+		break;
+	case '4':
+	case '5':
+		echo "es otoño";
+		break;
+	case '6':
+		if(date("d")>20)
+			echo "es invierno";
+		else
+			echo "es otoño";
+		break;
+	case '7':
+	case '8':
+		echo "es invierno";
+		break;
+	case '9':
+		if(date("d")>20)
+			echo "es primavera";
+		else
+			echo "es invierno";
+		break;
+	case '10':
+	case '11':
+		echo "es primavera";
+		break;
+	case '12':
+		if(date("d")>20)
+			echo "es verano";
+		else
+			echo "es primavera";
+		break;
+	default:
+		break;
+}
+
 //ej3
-echo "<br><br>--------------Ejercicio3--------------<br><br>";
+echo "<br><br>--------------Ejercicio 3--------------<br><br>";
 
 
 $a=rand(1,20);
